c8385a496ceb51960baeb8090b287e4394d7f1aa
marrabl <162188192+marrabl@users.noreply.github.com>
2025-04-23 16:30:45 +0200

    Add recursive methode to crawl links

--- src/main/java/org/example/WebCrawler.java
+++ src/main/java/org/example/WebCrawler.java
@@ -1,11 +1,22 @@
 package org.example;
 
-import java.util.List;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class WebCrawler {
     private final String url;
     private final int depth;
     private final List<String> domains;
+
+    private final Set<String> visitedLinks = new HashSet<>();   // set because no duplicates allowed
+    private final List<String> outputLines = new ArrayList<>();
 
     public WebCrawler(String url, int depth, List<String> domains) {
         this.url = url;
@@ -13,15 +24,50 @@ public class WebCrawler {
         this.domains = domains;
     }
 
-    public String getUrl() {
-        return url;
+    // recursive methode
+    public void run() {
+        crawl(0, url);
     }
 
-    public int getDepth() {
-        return depth;
+    private void crawl(int currentDepth, String url) {
+        if (currentDepth > depth || visitedLinks.contains(url) || !isDomainAllowed(url)) {
+            return;
+        }
+
+        visitedLinks.add(url);
+
+        Document doc = request(url);
+        if (doc != null) {
+            for (Element link : doc.select("a[href]")) {
+
+                String nextLink = link.absUrl("href");
+                crawl(currentDepth + 1, nextLink);
+            }
+        }
     }
 
-    public List<String> getDomains() {
-        return domains;
+
+    public Document request(String url) {
+        try {
+            Connection con = Jsoup.connect(url);
+            Document doc = con.get();
+
+            if (con.response().statusCode() == 200) {
+                return doc;
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error requesting " + url + ": " + e.getMessage());
+        }
+        return null;
+    }
+
+    public boolean isDomainAllowed(String url) {
+        for (String domain : domains) {
+            if (url.contains(domain)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
