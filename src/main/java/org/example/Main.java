fad230ab9edb9d5a7cdace0808823d17076e6c89
marrabl <162188192+marrabl@users.noreply.github.com>
2025-04-23 17:27:34 +0200

    Update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if (args.length < 3) {
+            System.out.println("Usage: java Main <url> <depth> <allowedDomains (comma-separated)>");
+            return;
         }
+
+        String url = args[0];
+        int depth = Integer.parseInt(args[1]);
+        List<String> domains = Arrays.asList(args[2].split(","));
+
+        WebCrawler crawler = new WebCrawler(url, depth, domains);
+        crawler.run();
     }
 }
