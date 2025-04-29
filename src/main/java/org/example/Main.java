f566c50b625bccf67e39a8fff238324fc99bf8b9
marrabl <162188192+marrabl@users.noreply.github.com>
2025-04-29 15:46:57 +0200

    Add logger to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,15 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         if (args.length < 3) {
-            System.out.println("Usage: java Main <url> <depth> <allowedDomains (comma-separated)>");
+            logger.info("Usage: java Main <url> <depth> <allowedDomains (comma-separated)>");
             return;
         }
 
@@ -15,13 +19,16 @@ public class Main {
 
         try {
             depth = Integer.parseInt(args[1]);
+
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Depth has to be an integer.");
+            logger.log(Level.SEVERE, "Depth must be an integer.", e);
+            return;
         }
 
         List<String> domains = Arrays.asList(args[2].split(","));
-        WebCrawler crawler = new WebCrawler(url, depth, domains);
+        logger.info("Starting crawler with URL: " + url + ", depth: " + depth + ", allowed domains: " + domains);
 
+        WebCrawler crawler = new WebCrawler(url, depth, domains);
         crawler.run();
     }
 }
